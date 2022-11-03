9d1bcfae6fd58570711f1e72106e57371af9a412
neresleonardo <leonardo.borges.neres@gmail.com>
2022-11-02 21:21:21 -0300

    user map

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -7,18 +8,44 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final _transaction = [
+    Transaction(id: 't1', title: 'Cinema', value: 300, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Conta de Luz', value: 300, date: DateTime.now()),
+    Transaction(id: 't3', title: 'Academia', value: 300, date: DateTime.now()),
+    Transaction(id: 't4', title: 'Netflix', value: 300, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Suas despesas"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Color.fromARGB(255, 17, 167, 24),
+              child: Text('Gráfico'),
+            ),
+          ),
+          Column(
+            children: _transaction.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }

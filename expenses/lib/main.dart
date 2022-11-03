2fa9876377f582d0840cc14b2a6252af45c4f266
neresleonardo <leonardo.borges.neres@gmail.com>
2022-11-02 22:39:43 -0300

    ElevatedButton

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -13,10 +14,10 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   final _transaction = [
     Transaction(id: 't1', title: 'Cinema', value: 300, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de Luz', value: 300, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de Luz', value: 300, date: DateTime.now()),
     Transaction(id: 't3', title: 'Academia', value: 300, date: DateTime.now()),
     Transaction(id: 't4', title: 'Netflix', value: 300, date: DateTime.now()),
   ];
@@ -28,7 +29,6 @@ class MyHomePage extends StatelessWidget {
         title: Text("Suas despesas"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -41,9 +41,62 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transaction.map((tr) {
               return Card(
-                child: Text(tr.title),
-              );
+                  child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 1,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text('R\$ ${tr.value.toString()}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple[600])),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.purple[600]),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(tr.date),
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 190, 190, 190),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Título'),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Valor'),
+                ),
+                ElevatedButton(
+                  child: Text('Nova Transação'),
+                  onPressed: () {},
+                )
+              ]),
+            ),
           )
         ],
       ),
